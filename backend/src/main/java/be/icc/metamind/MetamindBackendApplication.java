d32da91acd5a88dbba9cc5e841d4af0a878ee2bb
Hatim34 <Hatim34@users.noreply.github.com>
2026-08-23 02:09:31 +0200

    Ajout de la structure backend

---
+++ backend/src/main/java/be/icc/metamind/MetamindBackendApplication.java
@@ -0,0 +1,13 @@
+package be.icc.metamind;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MetamindBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MetamindBackendApplication.class, args);
+	}
+
+}
